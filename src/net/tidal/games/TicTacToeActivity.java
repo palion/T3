e12fb513eb08bf53a9600d628ff85abf6479a671
Qi Ming Chen <qiming.chen@gmail.com>
2011-08-06 15:55:50 -0700

    initial check in for T3

---
+++ src/net/tidal/games/TicTacToeActivity.java
@@ -0,0 +1,15 @@
+package net.tidal.games;
+
+import net.tidal.R;
+import net.tidal.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TicTacToeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
